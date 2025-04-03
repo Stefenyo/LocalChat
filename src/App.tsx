1fa946f2c9f7f1d157e91edbcba1c810182aab51
Stephenio Abbot <stephenioabbott@gmail.com>
2025-04-03 13:22:15 -0400

    updated theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { type FC } from 'react';
-import { Theme } from '@radix-ui/themes';
-import { Chat } from '@/routes';
+import { type FC } from "react";
+import { Theme } from "@radix-ui/themes";
+import { Chat } from "@/routes";
 
 const App: FC = () => {
   return (
-    <Theme appearance="dark" accentColor="red" radius="none" scaling="100%" hasBackground={false}>
+    <Theme appearance="dark" accentColor="red" radius="medium" scaling="100%">
       <Chat />
     </Theme>
   );
