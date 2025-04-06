9091296c03ce03d91e8bd8471670c568d00ed2df
Stephenio Abbot <stephenioabbott@gmail.com>
2025-04-06 01:23:03 -0400

    updated background component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { type FC } from "react";
 import { Theme } from "@radix-ui/themes";
 import { Chat } from "@/routes";
+import {
+  ShaderGradientBg,
+  type ShaderGradientBgProps,
+} from "./components/ShaderGradientBg";
+
+const theme: ShaderGradientBgProps["color"] = "green";
 
 const App: FC = () => {
   return (
-    <Theme appearance="dark" accentColor="red" radius="full" scaling="100%">
+    <Theme appearance="dark" accentColor={theme} radius="full" scaling="100%">
       <Chat />
+      <ShaderGradientBg color={theme} brightness={1} />
     </Theme>
   );
 };
