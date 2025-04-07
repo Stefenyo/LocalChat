cf496c5d1a10fe294e27f220231936ee45a8b058
Stephenio Abbot <stephenioabbott@gmail.com>
2025-04-07 16:34:15 -0400

    removed redundant utils folder

--- src/utils/getResponse/promptTemplates/index.ts
+++
@@ -1,1 +0,0 @@
-export { generateCompletePrompt } from "./generateCompletePrompt";
--- src/utils/getResponse/promptTemplates/templates/index.ts
+++
@@ -1,2 +0,0 @@
-export { BASE_PROMPT_TEMPLATE } from "./base";
-export { TITLES_PROMPT_TEMPLATE } from "./titles";
