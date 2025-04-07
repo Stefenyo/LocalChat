8a4991207ec5cf9935a1f4dbf9252cf42197d213
Stephenio Abbot <stephenioabbott@gmail.com>
2025-04-07 13:59:27 -0400

    added list of installed models dropdown

--- src/routes/settings/language-model.tsx
+++ src/routes/settings/language-model.tsx
@@ -1,8 +1,31 @@
-import { Card } from "@radix-ui/themes";
+import { useGetOllamaModels } from "@/hooks/useGetOllamaModels";
+import { Card, Select } from "@radix-ui/themes";
 import { createFileRoute } from "@tanstack/react-router";
 
 const LanguageModelsComponent = () => {
-  return <Card variant="surface">Language model options coming soon</Card>;
+  const { modelList, isLoading } = useGetOllamaModels();
+
+  if (isLoading) return <Card variant="surface">Loading model ...</Card>;
+  if (!modelList || !modelList.length)
+    return <Card variant="surface">No models found</Card>;
+
+  return (
+    <Card variant="surface">
+      <Select.Root defaultValue="apple">
+        <Select.Trigger />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Models</Select.Label>
+            {modelList.map((model) => (
+              <Select.Item key={model} value={model}>
+                {model}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+    </Card>
+  );
 };
 
 const Route = createFileRoute("/settings/language-model")({
