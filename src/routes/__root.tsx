f1e75ab4581987f3dee88b50de1032c5f7254880
Stephenio Abbot <stephenioabbott@gmail.com>
2025-04-07 01:49:03 -0400

    updated root route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,20 @@
+import { type FC } from "react";
 import { Theme } from "@radix-ui/themes";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { createRootRoute, Outlet, useMatchRoute } from "@tanstack/react-router";
+// import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import {
   ShaderGradientBg,
   type ShaderGradientBgProps,
 } from "@/components/ShaderGradientBg";
+import { UserMenu } from "@/components/UserMenu";
 
-const theme: ShaderGradientBgProps["color"] = "green";
+const theme: ShaderGradientBgProps["color"] = "red";
 
-const Route = createRootRoute({
-  component: () => (
+const RouteComponent: FC = () => {
+  const matchRoute = useMatchRoute();
+  const isHomepage = !!matchRoute({ to: "/", fuzzy: false });
+
+  return (
     <>
       <Theme
         appearance="dark"
@@ -18,12 +23,17 @@ const Route = createRootRoute({
         scaling="100%"
         hasBackground={false}
       >
+        <UserMenu />
         <Outlet />
       </Theme>
-      <ShaderGradientBg color={theme} brightness={1} />
-      <TanStackRouterDevtools />
+      <ShaderGradientBg color={theme} brightness={isHomepage ? 1 : 0.6} />
+      {/* <TanStackRouterDevtools /> */}
     </>
-  ),
+  );
+};
+
+const Route = createRootRoute({
+  component: RouteComponent,
 });
 
 export { Route };
