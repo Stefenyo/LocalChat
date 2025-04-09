ff06552f4510289013e22b5f5847a82e560608a9
Stephenio Abbot <stephenioabbott@gmail.com>
2025-04-09 17:43:56 -0400

    added chat history to convo

--- src/hooks/useGetResponse/generateCompletePrompt.ts
+++ src/hooks/useGetResponse/generateCompletePrompt.ts
@@ -2,6 +2,9 @@ const combinePromptTemplates = (promptTemplates: string[]) => {
   return `
     ${promptTemplates.join("\n")}
 
+  keep the chat history in mind when answering the question.
+  The chat history is as follows:
+  {chat_history}
   With this information in mind, respond to the following input:
   {question}
   `;
