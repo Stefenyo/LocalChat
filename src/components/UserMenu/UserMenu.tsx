b5cefeed4d7e9313721ecd0947214e922b13fa10
Stephenio Abbot <stephenioabbott@gmail.com>
2025-04-09 01:50:34 -0400

    updated UserMenu component

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,37 +1,33 @@
-import { GearIcon } from "@radix-ui/react-icons";
-import { Button, DropdownMenu } from "@radix-ui/themes";
+import { FileTextIcon } from "@radix-ui/react-icons";
+import { Button, Tooltip } from "@radix-ui/themes";
 import { FC } from "react";
 import { useNavigate } from "@tanstack/react-router";
-import { settingsUrls } from "@/routes/prompt-templates/route";
 
 const UserMenu: FC = () => {
   const navigate = useNavigate();
   return (
-    <div style={{ position: "fixed", top: "1rem", right: "1rem" }}>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger>
-          <Button
-            variant="solid"
-            radius="large"
-            size="3"
-            style={{ cursor: "pointer" }}
-          >
-            <GearIcon style={{ width: "18px", height: "18px" }} />
-            <DropdownMenu.TriggerIcon />
-          </Button>
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content>
-          {settingsUrls.map((item) => (
-            <DropdownMenu.Item
-              key={item.url}
-              onClick={() => navigate({ to: item.url })}
-              style={{ cursor: "pointer" }}
-            >
-              {item.title}
-            </DropdownMenu.Item>
-          ))}
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
+    <div style={{ position: "fixed", top: "1.5rem", right: "1.5rem" }}>
+      <Tooltip content="Prompt Templates" side="right">
+        <Button
+          variant="ghost"
+          radius="large"
+          size="1"
+          style={{ cursor: "pointer" }}
+          onClick={() => navigate({ to: "/prompt-templates/base" })}
+        >
+          <FileTextIcon />
+        </Button>
+      </Tooltip>
+      {/* <Button
+        variant="ghost"
+        radius="large"
+        size="1"
+        style={{ cursor: "pointer" }}
+        onClick={() => navigate({ to: "/prompt-templates/base" })}
+      >
+        <FileTextIcon />
+        Prompt Templates
+      </Button> */}
     </div>
   );
 };
